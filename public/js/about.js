36fd31de9eab7dc896b8b287d18dba8b329d23f7
Tenneh Kanneh <tennehkannehofficial@gmail.com>
2025-02-27 09:49:59 -0700

    Rearranging files for optimal file structure; major decisions on how to navigate pages and other such things.

--- public/js/about.js
+++ public/js/about.js
@@ -1,6 +1,20 @@
-function aboutContent(contentID) {
-    document.getElementById('contentID').addEventListener('click', function() {
+const statementButton = document.getElementById('personalStatement');
+const blogButton = document.getElementById('blog');
+const otherButton = document.getElementById('otherThings');
+const contentDisplay = document.getElementById('contentDisplay');
 
-        console.log(contentID + 'clicked');
-    });
-}
+statementButton.addEventListener('click', () => {
+    fetch('personalstatement.html')
+        .then(response => response.text())
+        .then(html => {
+            contentDisplay.innerHTML = html;
+        });
+});
+
+blogButton.addEventListener('click', () => {
+    contentDisplay.innerHTML = '<h2>Blog Title</h2><p>Here is some blog content.</p>';
+});
+
+otherButton.addEventListener('click', () => {
+    contentDisplay.innerHTML = '<p>Other things content.</p>';
+});
